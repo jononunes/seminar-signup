900837aa0ce72e0706206eeb426fda0b5a7f6797
Stefan von Buddenbrock <stefan@von-buddenbrock.com>
2021-08-11 17:41:27 +0200

    Updated template with loading seminars and covid waiver link.

--- database/views.py
+++ database/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from database.models import Seminar
+
 
 def signup_form(request):
-    return render(request, 'database/signup_form.html')
+    context = {'seminars': Seminar.objects.all()}
+    return render(request, 'database/signup_form.html', context=context)
