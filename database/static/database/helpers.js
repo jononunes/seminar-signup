5ef4bc50613623e9e793dedb78395b0165428880
Stefan von Buddenbrock <stefan@von-buddenbrock.com>
2021-08-15 20:13:32 +0200

    Added javascript to calculate price.

---
+++ database/static/database/helpers.js
@@ -0,0 +1,37 @@
+function get_ticked_checkboxes() {
+    let allCheckboxes = document.querySelectorAll('[id^="seminarCheckBox"]')
+    let tickedCheckboxes = []
+
+    for (let i = 0; i < allCheckboxes.length; ++i) {
+        let checkbox = allCheckboxes[i];
+        if (checkbox.checked) tickedCheckboxes.push(checkbox)
+    }
+    return tickedCheckboxes;
+}
+
+function get_total_cost(tickedCheckboxes) {
+    let totalCost = 0.0;
+
+    for (let i = 0; i < tickedCheckboxes.length; i++) {
+        let checkboxCost = parseFloat(tickedCheckboxes[i].getAttribute("data-price"))
+        totalCost += checkboxCost
+    }
+
+    return totalCost.toFixed(2)
+}
+
+function reset_payment_button() {
+    const button = document.getElementById('paymentButton')
+    const text = document.getElementById('paymentText')
+    let tickedCheckboxes = get_ticked_checkboxes()
+
+    let has_selected_one_checkbox = tickedCheckboxes.length > 0
+
+    if (has_selected_one_checkbox) {
+        button.disabled = false;
+        text.textContent = "Total cost: R" + get_total_cost(tickedCheckboxes)
+    } else {
+        button.disabled = true;
+        text.textContent = "Please select at least one seminar"
+    }
+}
