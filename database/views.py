ed58bbd2ca022d18ceeee1a7248960aa85e81202
Stefan von Buddenbrock <stefan@von-buddenbrock.com>
2021-08-16 17:45:05 +0200

    Changed so that payfast is not a redirect.

--- database/views.py
+++ database/views.py
@@ -11,7 +11,8 @@ def signup_form(request):
     context = {
         'seminars': sorted(Seminar.objects.filter(date_and_time__gt=timezone.now()), key=lambda x: x.date_and_time),
         'merchant_id': settings.PAYFAST_MERCHANT_ID,
-        'merchant_key': settings.PAYFAST_MERCHANT_KEY
+        'merchant_key': settings.PAYFAST_MERCHANT_KEY,
+        'payfast_url': settings.PAYFAST_URL
     }
     return render(request, 'database/signup_form.html', context=context)
 
@@ -23,48 +24,39 @@ def initial_payment_info_is_valid(post_data):
 def register(request):
     # TODO: First check if the data is valid, either with some code or JS on the page...
     if request.method == "POST":
-        return_from_payment = False
+        # TODO: Think about if I should check if the person already exists
+        # Make the parent
+        parent = Person(first_name=request.POST['parentFirstName'],
+                        last_name=request.POST['parentLastName'],
+                        email_address=request.POST['parentEmail'],
+                        cellphone_number=request.POST['parentPhoneNumber'])
+        # Make the student
+        student = Person(first_name=request.POST['studentFirstName'],
+                         last_name=request.POST['studentLastName'],
+                         email_address=request.POST['studentEmail'],
+                         cellphone_number=request.POST['studentPhoneNumber'])
 
-        if return_from_payment:
-            # TODO: Think about if I should check if the person already exists
-            # Make the parent
-            parent = Person(first_name=request.POST['parentFirstName'],
-                            last_name=request.POST['parentLastName'],
-                            email_address=request.POST['parentEmail'],
-                            cellphone_number=request.POST['parentPhoneNumber'])
-            # Make the student
-            student = Person(first_name=request.POST['studentFirstName'],
-                             last_name=request.POST['studentLastName'],
-                             email_address=request.POST['studentEmail'],
-                             cellphone_number=request.POST['studentPhoneNumber'])
+        parent.save()
+        student.save()
 
-            parent.save()
-            student.save()
+        parent_accepts_waiver = 'parentCovidWaiver' in request.POST
+        student_accepts_waiver = 'studentCovidWaiver' in request.POST
 
-            parent_accepts_waiver = 'parentCovidWaiver' in request.POST
-            student_accepts_waiver = 'studentCovidWaiver' in request.POST
+        time_registered = timezone.now()
 
-            time_registered = timezone.now()
+        registration = Registration(parent_guardian=parent,
+                                    parent_guardian_accepts_waiver=parent_accepts_waiver,
+                                    child=student,
+                                    child_accepts_waiver=student_accepts_waiver,
+                                    additional_info=request.POST['additionalInfoTextArea'],
+                                    time_registered=time_registered)
 
-            registration = Registration(parent_guardian=parent,
-                                        parent_guardian_accepts_waiver=parent_accepts_waiver,
-                                        child=student,
-                                        child_accepts_waiver=student_accepts_waiver,
-                                        additional_info=request.POST['additionalInfoTextArea'],
-                                        time_registered=time_registered)
+        registration.save()
 
-            registration.save()
-
-            for ticked_seminar in request.POST:
-                if ticked_seminar.startswith('seminarCheckBox'):
-                    seminar_id = ticked_seminar.replace('seminarCheckBox', '')
-                    seminar = Seminar.objects.filter(id=seminar_id).first()
-                    registration.registered_seminars.add(seminar)
-
-        else:
-            if initial_payment_info_is_valid(request.POST):
-                return redirect(settings.PAYFAST_URL)
-            else:
-                messages.error("There was an error with your details. Please try again.")
+        for ticked_seminar in request.POST:
+            if ticked_seminar.startswith('seminarCheckBox'):
+                seminar_id = ticked_seminar.replace('seminarCheckBox', '')
+                seminar = Seminar.objects.filter(id=seminar_id).first()
+                registration.registered_seminars.add(seminar)
 
     return redirect("signup-form")
