49642d605c4318fffadffbd1b535fd8726fe587c
Stefan von Buddenbrock <stefan@von-buddenbrock.com>
2021-08-18 16:41:40 +0200

    Changed non default fields to take blank.

--- database/models.py
+++ database/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    email_address = models.EmailField()
-    cellphone_number = models.CharField(max_length=20, null=True)
+    email_address = models.EmailField(blank=True)
+    cellphone_number = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
