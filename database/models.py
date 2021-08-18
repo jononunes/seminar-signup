48fd8acda3e9b98d7085e96acd9eaca262edbc63
Stefan von Buddenbrock <stefan@von-buddenbrock.com>
2021-08-18 16:22:50 +0200

    Increasing length of cellphone number whatwhat.

--- database/models.py
+++ database/models.py
@@ -6,7 +6,7 @@ class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     email_address = models.EmailField()
-    cellphone_number = models.CharField(max_length=11, null=True)
+    cellphone_number = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
